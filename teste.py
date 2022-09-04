0f58b9fb63e3cc61552820cbc9154bdfeaa09bf3
reelegis <110792099+reelegis@users.noreply.github.com>
2022-09-04 12:48:03 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import time
+with st.spinner('Aguarde um momento...'):
+    time.sleep(5)
+st.balloons()
+
+st.text("Versão beta 🐟 v.0.0.2")
+
+st.text('Última atualização em 02/09/2022')
